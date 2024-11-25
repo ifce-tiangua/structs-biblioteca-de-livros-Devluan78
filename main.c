a8f96ffd78393f184734efd211a565147dae4014
Devluan78 <123420545+Devluan78@users.noreply.github.com>
2024-11-25 14:58:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,24 +10,25 @@ typedef struct{
      char editora[100];
 }Livro, *pLivro;
 
-pLivro livro_aloc(int qtde){
-    pLivro livros;
-    livros = malloc(qtde * sizeof(Livro));
+pLivro* livro_aloc(int qtde){
+    pLivro livros =(pLivro) malloc(qtde * sizeof(pLivro));
     return livros;
  }
 
-void livro_ler(pLivro livros, int qtde){
+void livro_ler(pLivro* livros, int qtde){
     for(int i=1;i<=qtde;i++){
          getchar();
          fgets(livros[i].nome,100,stdin);
+         getchar();
          fgets(livros[i].isbn,50,stdin);
+         getchar();
          scanf("%f\n",&livros[i].preco);
          scanf("%d\n",&livros[i].score);
          getchar();
          fgets(livros[i].editora,100,stdin);
     }
 }
-void livro_exibe(pLivro livros, int qtde){
+void livro_exibe(pLivro* livros, int qtde){
      for(int i=1;i<=qtde;i++){
         printf("Livro %d:\n",i);
         printf("Nome: %s\n",livros[i].nome);
@@ -39,7 +40,7 @@ void livro_exibe(pLivro livros, int qtde){
 
      }
 }
-void livro_desaloca(pLivro livros){
+void livro_desaloca(pLivro* livros){
      free(livros);
 }
 
@@ -47,14 +48,17 @@ int main(){
   setlocale(LC_ALL,"Portuguese");
 
     int qtde;
-    pLivro livros;
-    
+    pLivro* livros;
     scanf("%d",&qtde);
 
-    livros = livro_aloc(qtde);
-    livro_ler(livros,qtde);
-    livro_exibe(livros,qtde);
-    livro_desaloca(livros);
+    if(qtde == 0){
+       printf("Sem livros");
+    }else{   
+      livros = livro_aloc(qtde);
+      livro_ler(livros,qtde);
+      livro_exibe(livros,qtde);
+      livro_desaloca(livros);
+    }    
 
     return 0;
 }
